43d8c2cc00dbbc6b94319100d6fdfa2de2046ed9
unknown <dawitgurmu5@gmail.com>
2019-10-04 14:30:21 +0000

    commit 2

---
+++ news.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="X-UA-Compatible" content="IE-chrome">
+    <meta name="viewport" content="width = device-width,initial-scale = 1 ">
+    <title>Ethiopian Hsitory </title>
+    <!--compiled and minified CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="include/index.css">
+    <!--Jquery library-->
+    <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+    <!-- Popper JS -->
+    <script type="text/javascript" src="popper.min.js"></script>
+    <!--compiled JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+
+
+    <?php require 'include/header.php'; ?>
+    <br><br>
+    <br>
+
+<body>
+<div class="container">
+<div class="row">
+<div class="card  bg-primary jam  col-xs-12 col-sm-12 col-md-12  col-lg-12"  >
+
+
+  <h1 class="display-3  text-center hedertext">Ethio Hsitory News </h1>
+
+  <p class="lead text-center hedertext"> This weeks headlines.</p>
+  
+ </div>
+ </div>
+</div>
+<br/>
+<div class="container">
+ <div class="row">
+<div class=" col-xs-12 col-sm-12  col-md-4 col-lg-4col-xl-4 " >
+    <div class="card jam2 ">
+        <h4 class="hedertext">3.8-million-year-old skull in Ethiopia an evolution game-changer</h4>
+        <img src="https://www.aljazeera.com/mritems/imagecache/mbdxxlarge/mritems/Images/2019/8/29/b2b122e53a6e4bce98d8c67cc71724e6_18.jpg" class="card-img-top" alt="...">
+        <p class="hedertext" >3.8-million-year-old skull in Ethiopia an evolution game-changer
+       
+              Major discovery 'looks set to become another celebrated icon of human evolution' after Lucy in 1974, scientist says.</p>
+              <p>by Aljazeera</p>
+            <a href="https://www.aljazeera.com/news/2019/08/38-million-year-skull-ethiopia-evolution-game-changer-190829055202695.html" target="_blank" class="btn btn-dark">Read Source </a>
+    </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-12  col-md-4 col-lg-4 col-xl-4 " >
+    <div class="card jam2">
+    
+    <h4 class="hedertext">Meaza Ashenafi: Judging Ethiopia's Future</h4>
+    <iframe src='https://players.brightcove.net/665003303001/SJg0bzqkZ_default/index.html?videoId=6082543135001&usrPersonaAds=false' allowfullscreen frameborder=0></iframe>
+            
+              
+    <p class="hedertext">
+    
+Meet Ethiopia's first female chief justice as she takes on the challenges of transforming the country's 
+        </p>
+    <p>by Aljazeera</p>
+        <a href="https://www.aljazeera.com/programmes/my-ethiopia/2019/09/meaza-ashenafi-judging-ethiopia-future-190902094956760.html" target="_blank" class="btn btn-dark">Read source </a>
+    </div>
+</div>
+    <div class=" col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4" >
+    <div class="card jam2">
+    <h4 class="hedertext">  Ethiopia's Meskel festival: Bonfires, robes and crosses </h4> 
+    <img src= https://ichef.bbci.co.uk/news/976/cpsprodpb/CB20/production/_103600025_cross.jpg class="card-img-top" alt="...">
+    <p class="hedertext">
+    Ethiopians are celebrating the annual Meskel festival, the first big festival of the Ethiopian religious year.
+
+According to Ethiopian Orthodox Christian tradition, the national holiday marks the finding of the cross that Jesus was crucified on
+    </p>
+    <p>by BBC</p>
+        <a href="https://www.bbc.com/news/world-africa-45665901" target="_blank" class="btn btn-dark">Read sourece </a>
+      
+</div>
+
+    </div>
+    
+    <div class="container">
+    <div class="d-lg-none">
+    
+        <nav aria-label="...">
+            <ul class="pagination">
+                <li class="page-item disabled">
+                <li class="page-item"><a class="page-link" href="index.php">Previous</a></li>
+                <li class="page-item " aria-current="page">
+                    <a class="page-link" href="#">Top <span class="sr-only"></span></a>
+                </li>
+             
+                <li class="page-item">
+                    <a class="page-link" href="biography.php">Next</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    </div>
+    </div>
+     <div>
+
+
+  
+    
+    <br/>
+<footer  class="d-lg-none">
+<?php require 'include/footer.php'; ?>
+</footer>
+
+
